8a65104814ef0ef77a966baffdec536d438e9280
Tobias Melen <tobias.melen@gmail.com>
2022-05-13 07:46:00 +0200

    Always include react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(async ({ command }) => ({
-  plugins: [
-    command === "serve" && (await import("@vitejs/plugin-react")).default(),
-  ].filter(Boolean),
-}));
+export default defineConfig({
+  plugins: [react()],
+});
